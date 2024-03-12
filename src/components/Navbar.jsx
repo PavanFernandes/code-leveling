3e720a52e98627fb0fc4de3be9bbb30702e5f13d
kishor <kishork62006gmail.com>
2024-03-13 01:05:28 +0530

    added profile dropdown feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import logo from '../assets/images/logo.png'
+import Dropdown from '../components/Dropdown'
 
 function Navbar() {
     return (
@@ -19,7 +20,8 @@ function Navbar() {
                         <li><Link to="/rankings" className="hover:text-gray-300">Rankings</Link></li>
                     </ul> 
                     <div className="mx-10 flex">
-                        <img src="https://xsgames.co/randomusers/avatar.php?g=female" alt="Profile" className="h-10 rounded-full" />
+                        <Dropdown/> 
+                        
                 </div>
                 </div>
             </div>
