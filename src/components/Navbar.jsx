26008aa232d1647072c9a5d4229c0f3cd0a323e7
Samivan <pavanfernandes1@gmail.com>
2024-03-14 01:37:07 +0530

    refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,16 @@ import logo from '../assets/images/logo.png'
 import Profile from './Profile'
 
 function Navbar() {
+
+    const elements = [
+        {title : "Home", path : "/"},
+        {title : "Projects", path : "/projects"},
+        {title : "Learn", path : "/learn"},
+        {title : "Problems", path : "/problems"},
+        {title : "Compete", path : "/compete"},
+        {title : "Rankings", path : "/rankings"}
+    ]
+
     return (
         <Fragment>
             <div className="bg-gradient-to-r from-[#CDC07F] via-[#EAE6BC] to-[#B7C7C5] py-5 mx-auto h-16 flex justify-around items-center shadow-lg">
@@ -12,14 +22,12 @@ function Navbar() {
                     <h1 className="mx-2 font-bold">CODE LEVELING</h1>
                 </div>
                 <div className='flex justify-end items-center'>
-                    <ul className="flex space-x-4 font-bold">
-                        <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
-                        <li><Link to="/projects" className="hover:text-gray-300">Projects</Link></li>
-                        <li><Link to="/learning" className="hover:text-gray-300">Learning</Link></li>
-                        <li><Link to="/compete" className="hover:text-gray-300">Compete</Link></li>
-                        <li><Link to="/rankings" className="hover:text-gray-300">Rankings</Link></li>
+                    <ul className="flex space-x-4">
+                        {elements.map(e => (
+                            <li key={e.title}><Link to={e.path} className="font-normal hover:text-gray-400">{e.title}</Link></li>
+                        ))}
                     </ul> 
-                    <div className="mx-10 flex">
+                    <div className="mx-4 flex">
                         <Profile avatar={"https://xsgames.co/randomusers/avatar.php?g=female"}/>
                 </div>
                 </div>
